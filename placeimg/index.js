4104278fd4ec356136c5999ef0c5152e463fefbf
Francis Lewis <francislewis@users.noreply.github.com>
2015-08-26 02:05:38 +0100

    I hope this works

--- placeimg/index.js
+++ placeimg/index.js
@@ -1,12 +1,28 @@
-//Get x value
-var valx = document.getElementById('valx').value;
+function getValues() {
 
-//Get y value
-var valy = document.getElementById('valy').value;
+  //Get x value
+  var valx = document.getElementById('valx').value;
 
-// Define the string
-var string = "<?xml version="1.0" encoding="UTF-8" standalone="yes"?><svg xmlns="http:/\/www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" preserveAspectRatio="none"><defs><style type="text/css"><![CDATA[#holder_14f672961df text { fill:#AAAAAA;font-weight:bold;font-family:Arial, Helvetica, Open Sans, sans-serif, monospace;font-size:25pt } ]]></style></defs><g id="holder_14f672961df"><rect width="500" height="500" fill="#EEEEEE"/><g><text x="185.1328125" y="261.1">500x500</text></g></g></svg>";
+  //Get y value
+  var valy = document.getElementById('valy').value;
+  
+  //Call next function
+  addToString();
+}
+
+function addToString() {
+  // Define the string
+  var string = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+  <svg xmlns="http://www.w3.org/2000/svg" width="${valx}" height="${valy}" viewBox="0 0 ${valx} ${valy}" preserveAspectRatio="none">
+  <defs><style type="text/css"><![CDATA[#holder_14f672961df text { fill:#AAAAAA;font-weight:bold;font-family:Arial, Helvetica, Open Sans, sans-serif, monospace;font-size:25pt } ]]></style></defs>
+  <g id="holder_14f672961df"><rect width="${valx}" height="${valy}" fill="#EEEEEE"/><g>
+  <text x="185.1328125" y="261.1">${valx}x${valy}</text></g></g></svg>`;
+}
 
 // Encode the String
 var encodedString = btoa(string);
-console.log(encodedString); // Outputs: "SGVsbG8gV29ybGQh"
+
+//Add html stuff to encoded data and place in html
+document.getElementById('final').innerHTML = `<img src="data:image/svg+xml;base64,${encodedString}">`
+
+
