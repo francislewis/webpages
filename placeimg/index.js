2952a200e7a908159379fe968c19d40d532fca91
Francis Lewis <francislewis@users.noreply.github.com>
2015-08-26 02:31:24 +0100

    Update index.js

--- placeimg/index.js
+++ placeimg/index.js
@@ -1,5 +1,7 @@
 var valx
 var valy
+var string
+
 console.log("defined");
 function getValues() {
 
@@ -9,14 +11,16 @@ function getValues() {
   //Get y value
   var valy = document.getElementById('valy').value;
  console.log("got values");
+ stringShit();
 }
 
+function stringShit() {
   // Define the string
   var string = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
   <svg xmlns="http://www.w3.org/2000/svg" width="${valx}" height="${valy}" viewBox="0 0 ${valx} ${valy}" preserveAspectRatio="none">
   <defs><style type="text/css"><![CDATA[#holder_14f672961df text { fill:#AAAAAA;font-weight:bold;font-family:Arial, Helvetica, Open Sans, sans-serif, monospace;font-size:25pt } ]]></style></defs>
   <g id="holder_14f672961df"><rect width="${valx}" height="${valy}" fill="#EEEEEE"/><g>
-  <text x="185.1328125" y="261.1">${valx}x${valy}</text></g></g></svg>`;
+  <text x="185.1328125" y="261.1">${valx}x${valy}</text></g></g></svg>`; }
 console.log("made string");
 // Encode the String
 var encodedString = btoa(string);
